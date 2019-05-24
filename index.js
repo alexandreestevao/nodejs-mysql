1c29d8224f49b7163e9b3a59a077071b77dfe897
Alexandre Estevão <alexandresestevao@gmail.com>
2019-05-24 10:17:19 -0300

    Instalação do Express e criação de rotas no NodeJs

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var http = require('http');
+const express = require("express"); //instanciando o módulo express
 
-http.createServer(function(req, res){
-  res.end("Gerenciador Financeiro")
-}).listen(8080);
+const app = express(); //clonando do módulo express para uma variável
+
+//página raiz
+app.get("/", function(req, res){
+  res.send("Gerenciador Financeiro");
+});
+
+//página de contato
+app.get("/contato", function(req, res){
+  res.send("Página de contato");
+});
+
+//localhost:3000
+app.listen(3000); //servidor iniciado na porta 3000
